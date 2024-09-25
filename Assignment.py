be597e107fa6553930fc8ff4340229475b30cf1c
Yusuf Ahmed <79456680+MoeJango@users.noreply.github.com>
2024-09-25 19:04:47 +0200

    V3

--- Assignment.py
+++ Assignment.py
@@ -1,11 +1,6 @@
 import numpy as np
 
-def zerosCovered(costArr, coveredArr, starredZeros):
-    for i in range(11):
-            for j in range(11):
-                if starredZeros[i][j] == 1:
-                    coveredArr[:, j] = 1
-
+def zerosCovered(costArr, coveredArr):
     for i in range(11):
         for j in range(11):
             if costArr[i][j] == 0 and not coveredArr[i][j] == 1:
@@ -14,7 +9,7 @@ def zerosCovered(costArr, coveredArr, starredZeros):
 
 
 def coverings(costArr, coveredArr, starredZeros, primedZeros):
-    isCovered = zerosCovered(costArr, coveredArr, starredZeros)
+    isCovered = zerosCovered(costArr, coveredArr)
     while not isCovered == -1:
         primedZeros[isCovered[0]][isCovered[1]] = 1
         if np.any(starredZeros[isCovered[0], :]):
@@ -50,18 +45,20 @@ def coverings(costArr, coveredArr, starredZeros, primedZeros):
                         starredZeros[i][j] = 1
             primedZeros = np.zeros((11, 11))
             coveredArr = np.zeros((11, 11))
-            isCovered = zerosCovered(costArr, coveredArr, starredZeros)
-            break
-        isCovered = zerosCovered(costArr, coveredArr, starredZeros)
+            for i in range(11):
+                for j in range(11):
+                    if starredZeros[i][j] == 1:
+                        coveredArr[:, j] = 1
+        isCovered = zerosCovered(costArr, coveredArr)
+
     
-    lines = 0
+    starred = 0
     for i in range(11):
-        if np.all(coveredArr[i, :] == 1):
-            lines += 1
-    for j in range(11):
-        if np.all(coveredArr[:, j] == 1):
-            lines += 1
-    return lines
+        for j in range(11):
+            if starredZeros[i][j] == 1:
+                starred += 1
+    
+    return starred
 
 
 def role_assignment(teammate_positions, formation_positions): 
@@ -90,47 +87,31 @@ def role_assignment(teammate_positions, formation_positions):
                 starredZeros[i][j] = 1
                 break
 
-    lines = coverings(costArr, coveredArr, starredZeros, primedZeros)
-    while lines < 11:
+    for i in range(11):
+            for j in range(11):
+                if starredZeros[i][j] == 1:
+                    coveredArr[:, j] = 1 
+
+    starred = coverings(costArr, coveredArr, starredZeros, primedZeros)
+    
+    while starred < 11:
+    
         minVal = np.min(costArr[np.where(coveredArr == 0)])
+    
         for i in range(11):
             if np.all(coveredArr[i, :] == 0):
                 costArr[i, :] -= minVal
             if np.all(coveredArr[:, i] == 1):
                 costArr[:, i] += minVal
-        lines = coverings(costArr, coveredArr, starredZeros, primedZeros)
+        starred = coverings(costArr, coveredArr, starredZeros, primedZeros)
         
 
     point_preferences = {}
 
-    rowMask = np.zeros(11)
-    colMask = np.zeros(11)
-    
-
-    while len(point_preferences) < 11:
-        minRow = -1
-        minRowCount = 100
-        for i in range(11):
-            if rowMask[i] == 1:
-                continue
-            thisRowCount = 0
-            for j in range(11):
-                if colMask[j] == 1:
-                    continue
-                if costArr[i][j] == 0:
-                    thisRowCount += 1
-            if thisRowCount < minRowCount:
-                minRowCount = thisRowCount
-                minRow = i
-        
-        rowMask[minRow] = 1
+    for i in range(11):
         for j in range(11):
-            if colMask[j] == 1:
-                continue
-            if costArr[minRow][j] == 0:
-                colMask[j] = 1
-                point_preferences[minRow + 1] = formation_positions[j]
-                break
+            if starredZeros[i][j] == 1:
+                point_preferences[i+1] = formation_positions[j]
 
     return point_preferences
 
@@ -152,31 +133,37 @@ def pass_reciever_selector(player_unum, teammate_positions, final_target):
     return target
 
 
-teammate_positions = np.array([ np.array([3.74540119, 9.50714306]),
-  np.array([7.31993942, 5.98658484]),
-  np.array([1.5601864, 1.5599452]),
-  np.array([0.58083612, 8.66176146]),
-  np.array([6.01115012, 7.08072578]),
-  np.array([0.20584494, 9.69909852]),
-  np.array([8.32442641, 2.12339111]),
-  np.array([1.81824967, 1.8340451 ]),
-  np.array([3.04242243, 5.24756432]),
-  np.array([4.31945019, 2.9122914 ]),
-  np.array([6.11852895, 1.39493861]) ])
+teammate_positions = np.array([
+    np.array([4, 5]),   # Player 1
+    np.array([8, 3]),   # Player 2
+    np.array([7, 2]),   # Player 3
+    np.array([1, 6]),   # Player 4
+    np.array([5, 7]),   # Player 5
+    np.array([6, 4]),   # Player 6
+    np.array([2, 8]),   # Player 7
+    np.array([3, 9]),   # Player 8
+    np.array([7, 7]),   # Player 9
+    np.array([5, 3]),   # Player 10
+    np.array([9, 1]),   # Player 11
+])
 
 # Formation positions as 11 2-element numpy arrays
-formation_positions = np.array([ np.array([2.92144649, 3.66361843]),
-  np.array([4.56069984, 7.85175961]),
-  np.array([1.99673782, 5.14234438]),
-  np.array([5.92414569, 0.46450413]),
-  np.array([6.07544852, 1.70524124]),
-  np.array([0.65051593, 9.48885537]),
-  np.array([9.65632033, 8.08397348]),
-  np.array([3.04613769, 0.97672114]),
-  np.array([6.84233027, 4.40152494]),
-  np.array([1.22038235, 4.9517691 ]),
-  np.array([0.34388521, 9.09320402]) ])
+formation_positions = np.array([
+    np.array([1, 5]),   # Formation position 1
+    np.array([7, 3]),   # Formation position 2
+    np.array([4, 2]),   # Formation position 3
+    np.array([2, 7]),   # Formation position 4
+    np.array([8, 6]),   # Formation position 5
+    np.array([3, 4]),   # Formation position 6
+    np.array([5, 8]),   # Formation position 7
+    np.array([6, 5]),   # Formation position 8
+    np.array([7, 4]),   # Formation position 9
+    np.array([9, 3]),   # Formation position 10
+    np.array([2, 6])    # Formation position 11
+])
 
+print("hello")
 p = role_assignment(teammate_positions, formation_positions)
 print(p)
+
 
