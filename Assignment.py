67070f5fe7b68cdf61b10f8fcf7f8b20c9b112a4
Yusuf Ahmed <79456680+MoeJango@users.noreply.github.com>
2024-09-26 19:25:44 +0200

    V4

--- Assignment.py
+++ Assignment.py
@@ -15,7 +15,9 @@ def coverings(costArr, coveredArr, starredZeros, primedZeros):
         if np.any(starredZeros[isCovered[0], :]):
             for k in range(11):
                 if starredZeros[isCovered[0]][k] == 1:
-                    coveredArr[:, k] = 0
+                    for c in range(11):
+                        if not np.all(coveredArr[c, :] == 1):
+                            coveredArr[c][k] = 0
                     coveredArr[isCovered[0], :] = 1
                     break
         else:
@@ -43,8 +45,8 @@ def coverings(costArr, coveredArr, starredZeros, primedZeros):
                         starredZeros[i][j] = 0
                     if path[i][j] == 1 and primedZeros[i][j] == 1:
                         starredZeros[i][j] = 1
-            primedZeros = np.zeros((11, 11))
-            coveredArr = np.zeros((11, 11))
+            primedZeros.fill(0)
+            coveredArr.fill(0)
             for i in range(11):
                 for j in range(11):
                     if starredZeros[i][j] == 1:
@@ -95,11 +97,10 @@ def role_assignment(teammate_positions, formation_positions):
     starred = coverings(costArr, coveredArr, starredZeros, primedZeros)
     
     while starred < 11:
-    
         minVal = np.min(costArr[np.where(coveredArr == 0)])
     
         for i in range(11):
-            if np.all(coveredArr[i, :] == 0):
+            if not np.all(coveredArr[i, :] == 1):
                 costArr[i, :] -= minVal
             if np.all(coveredArr[:, i] == 1):
                 costArr[:, i] += minVal
@@ -131,39 +132,5 @@ def pass_reciever_selector(player_unum, teammate_positions, final_target):
         target = final_target 
     
     return target
-
-
-teammate_positions = np.array([
-    np.array([4, 5]),   # Player 1
-    np.array([8, 3]),   # Player 2
-    np.array([7, 2]),   # Player 3
-    np.array([1, 6]),   # Player 4
-    np.array([5, 7]),   # Player 5
-    np.array([6, 4]),   # Player 6
-    np.array([2, 8]),   # Player 7
-    np.array([3, 9]),   # Player 8
-    np.array([7, 7]),   # Player 9
-    np.array([5, 3]),   # Player 10
-    np.array([9, 1]),   # Player 11
-])
-
-# Formation positions as 11 2-element numpy arrays
-formation_positions = np.array([
-    np.array([1, 5]),   # Formation position 1
-    np.array([7, 3]),   # Formation position 2
-    np.array([4, 2]),   # Formation position 3
-    np.array([2, 7]),   # Formation position 4
-    np.array([8, 6]),   # Formation position 5
-    np.array([3, 4]),   # Formation position 6
-    np.array([5, 8]),   # Formation position 7
-    np.array([6, 5]),   # Formation position 8
-    np.array([7, 4]),   # Formation position 9
-    np.array([9, 3]),   # Formation position 10
-    np.array([2, 6])    # Formation position 11
-])
-
-print("hello")
-p = role_assignment(teammate_positions, formation_positions)
-print(p)
 
 
